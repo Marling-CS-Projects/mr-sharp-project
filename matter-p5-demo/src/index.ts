4d7aaf489e34aa4c4d1f31d4044e815c5c643592
Joe Sharp <ratracejoe@hotmail.com>
2021-12-08 13:55:01 +0000

    added demo of array of objects

--- matter-p5-demo/src/index.ts
+++ matter-p5-demo/src/index.ts
@@ -4,6 +4,7 @@ import { Engine, World, Bodies, Events, IEventCollision } from 'matter-js';
 
 import Player from './Player';
 import Level from './Level';
+import * as Matter from 'matter-js';
 
 const LEVEL_WIDTH = 10000;
 
@@ -13,6 +14,9 @@ let sketch = function (p: p5) {
     var ground: Matter.Body;
     let player: Player;
     let level: Level;
+
+    // Demo of array of bodies
+    let skyDecorations: Matter.Body[];
 
     p.setup = function () {
         p.createCanvas(700, 410);
@@ -24,6 +28,12 @@ let sketch = function (p: p5) {
         level = new Level(p, engine);
 
         World.add(engine.world, [ground]);
+
+        skyDecorations = []
+        skyDecorations.push(Bodies.rectangle(40, 40, 50, 60, { isStatic: true }))
+        skyDecorations.push(Bodies.rectangle(150, 50, 50, 60, { isStatic: true }))
+        skyDecorations.push(Bodies.rectangle(250, 30, 50, 60, { isStatic: true }))
+        skyDecorations.push(Bodies.rectangle(350, 25, 50, 60, { isStatic: true }))
 
         // Setup some collision detection
         Events.on(engine, 'collisionStart', (event: IEventCollision<Engine>) => {
@@ -78,6 +88,18 @@ let sketch = function (p: p5) {
             p.vertex(vertex.x, vertex.y);
         })
         p.endShape(p.CLOSE);
+
+        // Draw sky decorations in array
+        p.fill("yellow")
+        skyDecorations.forEach(skyDecoration => {
+            p.beginShape()
+
+            skyDecoration.vertices.forEach(v => {
+                p.vertex(v.x, v.y)
+            })
+
+            p.endShape(p.CLOSE)
+        })
 
         p.pop();
     };
